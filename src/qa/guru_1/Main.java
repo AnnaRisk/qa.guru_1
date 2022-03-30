962877427a5c60c8a06446d28d3b11315b67c97e
24-002580\user <annarainchik@gmail.com>
2022-03-30 14:12:05 +0300

    test branch new!

--- src/qa/guru_1/Main.java
+++ src/qa/guru_1/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, qA.GUrU2!");
-        System.out.println("test branch");
+        System.out.println("test branch new!");
 
         System.out.println("new line test");
+
     }
 }
