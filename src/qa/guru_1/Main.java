c5104f9e412233d2dab12d5805f44632a17d51ad
24-002580\user <annarainchik@gmail.com>
2022-03-30 14:04:37 +0300

    add new line new new

--- src/qa/guru_1/Main.java
+++ src/qa/guru_1/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, qA.GUrU2!");
-        System.out.println("test conflict");
+        System.out.println("test conflict new new");
     }
 }
