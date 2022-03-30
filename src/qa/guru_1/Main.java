4c9861d85d397458a41f962842e97516cb5a2946
24-002580\user <annarainchik@gmail.com>
2022-03-30 14:16:51 +0300

    conflict add

--- src/qa/guru_1/Main.java
+++ src/qa/guru_1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello, qA.GUrU2!");
+        System.out.println("Hello, qA.GUrU2! add conflict");
         System.out.println("test branch new!");
 
         System.out.println("new line test");
